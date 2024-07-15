d1784a70ab0744508c933b6da87ae74d2efc14e4
root <root@srv561425.hstgr.cloud>
2024-07-15 14:04:10 +0000

    final 1

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,16 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'frontend/dist', // This should match the directory from where you are serving static files
-    assetsDir: 'assets', // Where to place the static assets (relative to `outDir`)
-  },
   server: {
-    port: 3001, // Ensure this matches your backend server port if needed
-  },
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+        rewrite: path => path.replace(/^\/api/, '')
+      }
+    }
+  }
 });
+
