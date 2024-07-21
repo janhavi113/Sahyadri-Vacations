c90a10ff1647534bf7ffc5c45b8e3683743f5f4f
janhavi113 <49247219+janhavi113@users.noreply.github.com>
2024-07-21 22:56:04 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -131,7 +131,7 @@ app.post('/admin-login', async (req, res) => {
 // Set up storage engine
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-      cb(null, 'uploads/'); // Directory to save uploaded files
+      cb(null, 'public/Images/'); // Directory to save uploaded files
   },
   filename: (req, file, cb) => {
       cb(null, Date.now() + path.extname(file.originalname)); // File name with timestamp
