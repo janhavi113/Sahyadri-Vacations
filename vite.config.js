e752688c1238ec65ddc01715ab830bdef0f89d7a
janhavi113 <49247219+janhavi113@users.noreply.github.com>
2024-07-24 22:03:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://157.173.222.166:3001',
+        target: 'http://localhost:3001',
         changeOrigin: true,
         secure: false,
         ws: true,
