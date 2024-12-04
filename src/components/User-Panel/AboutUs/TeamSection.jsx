d9e66555a9465d6cd8f927aa32930a6f2e4a2965
janhavi113 <49247219+janhavi113@users.noreply.github.com>
2024-12-05 00:38:02 +0530

    commit

--- src/components/User-Panel/AboutUs/TeamSection.jsx
+++ src/components/User-Panel/AboutUs/TeamSection.jsx
@@ -15,12 +15,13 @@ const TeamSection = () => {
           <h2 className="h2">Pravin Sahane</h2>
           <div className="designation">Founder</div>
 
-          <p className="team-info">A passionate traveller and mountaineer. He has explored more than 350 treks in the Sahyadri mountain range. He is a great leader and skilled Mountaneer. He has successfully led more than 150 treks. He believe that travel has the power to transform lives, broaden horizons and is main tool to improve World Happiness Index.</p>
-          <a className="links" href="https://www.instagram.com/traveller_hrushi/" target="_blank" > <FontAwesomeIcon className="icon" icon={faSquareFacebook} style={{ color: "Orange", }} size='xl' />       </a>
-          <a className="links" href="https://www.instagram.com/traveller_hrushi/" target="_blank" > <FontAwesomeIcon className="icon" icon={faInstagram} style={{ color: "Orange", }} size='xl' />      </a>
-          <a className="links" href="https://www.instagram.com/traveller_hrushi/" target="_blank" > <FontAwesomeIcon className="icon" icon={faYoutube} style={{ color: "Orange", }} size='xl' />   </a>
-          <a className="links" href="https://www.instagram.com/traveller_hrushi/" target="_blank" > <FontAwesomeIcon className="icon" icon={faLinkedinIn} style={{ color: "Orange", }} size='xl' />   </a>
-        </div>
+          <p className="team-info">
+          As the founder of Sahyadri Vacations and Adventures, He has explored more than 350 treks in Sahyadri mountain ranges and travel a lot in all over India. With a love for adventure and exploration, He leads a team who inspire others to step out of their comfort zones and experience the world through trekking and travel. His mission is crafting unforgettable journeys that connect people with nature and adventure. He is great leader and Mountaineer. He is deeply passionate about traveling and believe that every journey has the power to transform lives, broaden horizon and main tool to improve World Happiness Index.
+          </p>
+           <a className="links" href="https://www.facebook.com/profile.php?id=100090509509892&mibextid=JRoKGi" target="_blank" > <FontAwesomeIcon className="icon" icon={faSquareFacebook} style={{ color: "Orange", }} size='xl' />       </a>
+          <a className="links" href="https://www.instagram.com/ek_awaraaa?igsh=NmowZTBrODBxcmpp" target="_blank" > <FontAwesomeIcon className="icon" icon={faInstagram} style={{ color: "Orange", }} size='xl' />      </a>
+          <a className="links" href="https://youtube.com/@ekawaraa?si=YXubYwoPh-jvgyR4" target="_blank" > <FontAwesomeIcon className="icon" icon={faYoutube} style={{ color: "Orange", }} size='xl' />   </a>
+         </div>
 
       </div>
     </div>
