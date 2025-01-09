a6de7e9f0bc7a2e50f80715650a88ae00d7a6f32
janhavi113 <49247219+janhavi113@users.noreply.github.com>
2025-01-09 16:15:11 +0530

    Update ShowEventDetails.jsx

--- src/components/User-Panel/ShowEventDetails/ShowEventDetails.jsx
+++ src/components/User-Panel/ShowEventDetails/ShowEventDetails.jsx
@@ -78,6 +78,7 @@ const ShowEventDetails = () => {
   const [selectedLocation, setSelectedLocation] = useState(null);
   const [showTermsAndConditions, setShowTermsAndConditions] = useState(false);
   const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [paynowButtonDisabled, setPayNowButtonDisabled] = useState(false);
   const [batchFull, setBatchFull] = useState(false);
   const [couponCode, setCouponCode] = useState('');
   const [coupons, setCoupons] = useState([]);
@@ -103,13 +104,16 @@ const ShowEventDetails = () => {
     let price = 0;
     if (option == 'Pune to Pune') {
       price = foundRecord.eventCostPerPerson;
+      console.log('fiest ');
       setShowTermsAndConditions(false);
     } else if (option == 'Mumbai to Mumbai') {
       price = foundRecord.eventCostPerPersonFromMumbai;
+      console.log('fiest 1');
       setShowTermsAndConditions(false);
     } else {
       setSelectedLocation(option);
       setShowTermsAndConditions(true);
+      console.log('fiest 2');
       price = foundRecord.b2bPrice;
     }
     //console.log('price---', price);
@@ -135,13 +139,14 @@ const ShowEventDetails = () => {
     if (event.target.value == "partial") {
       let price = Number(selectedBatch.partialBookingAmount) * Number(noOfTrekkers);
       setConvenienceFee(Number(price) * 0.015);
-      price = price + (price * 0.015);
+      // price = price ;//+ (price * 0.015);
       setPartialPayment(price);
       setFinalPrice(price);
       setDiscount(0);
+      setCouponCode('');
       let remainingAmount = Number(selectedBatch.eventCostPerPerson) * Number(noOfTrekkers);
-      let remainingConvenienceFee = Number(remainingAmount) * 0.015;
-      remainingAmount = remainingAmount + remainingConvenienceFee;
+      //let remainingConvenienceFee = Number(remainingAmount) * 0.015;
+      //remainingAmount = remainingAmount ;//+ remainingConvenienceFee;
       setRemainingAmount(remainingAmount);
       setPrice(Number(selectedBatch.partialBookingAmount));
 
@@ -153,8 +158,9 @@ const ShowEventDetails = () => {
         price = Number(price) - Number(discount);
       } else {
         setDiscount(0);
+        setCouponCode('');
       }
-      price = price + (price * 0.015);
+      // price = price ; // + (price * 0.015);
       setActualPrice(Number(selectedBatch.eventCostPerPerson) * Number(noOfTrekkers));
       setFinalPrice(price);
       setPrice(Number(selectedBatch.eventCostPerPerson));
@@ -213,26 +219,29 @@ const ShowEventDetails = () => {
 
   // Handle blur to toggle the value
   const handleCheckboxBlur = () => {
+
     if (selectedLocation == null && eventType != 'CampingEvent') {
+    
       setError("dateError", {
         type: "manual",
         message: "Please select Pickup Location and accept terms & condition to proceed",
       })
-      setButtonDisabled(false);
+      setPayNowButtonDisabled(false);
       if (termsChecked) {
         setValue("termsAndconditions", false);
       }
     } else {
-      clearErrors('dateError');
-      setButtonDisabled(false);
+        setPayNowButtonDisabled(true);
+        clearErrors('dateError');
+     
     }
   }
 
   const onSubmit = async (data) => {
+    
     setIsLoading(true); // Set loading to true before starting the request
     try {
       if (buttonClick == 'pay-now') {
-
         const formData = new FormData();
         formData.append("numberOfPeoples", noOfTrekkers);
         formData.append("amountPaid", Number(finalPrice));
@@ -314,6 +323,7 @@ const ShowEventDetails = () => {
   const handleSelection = (event) => {
     setSelectedLocation(event.target.value);
     setShowTermsAndConditions(true);
+    console.log('fiest 4');
     clearErrors('dateError');
   };
 
@@ -330,6 +340,7 @@ const ShowEventDetails = () => {
         final_Price = Number(final_Price) - Number(discount);
       } else {
         setDiscount(0);
+        setCouponCode('');
       }
       if (paymentOption == 'partial') {
         setPartialPayment(final_Price);
@@ -362,6 +373,7 @@ const ShowEventDetails = () => {
         final_Price = Number(final_Price) - Number(discount);
       } else {
         setDiscount(0);
+        setCouponCode('');
       }
       setNoOfTrekkers(Number(count));
       setActualPrice(Number(amount));
@@ -410,6 +422,7 @@ const ShowEventDetails = () => {
 
     if (eventType == 'CampingEvent') {
       setShowTermsAndConditions(true);
+      console.log('fiest 5');
     }
 
     const Q = new Date();
@@ -565,15 +578,15 @@ const ShowEventDetails = () => {
       getAllRecord();
     }
 
-    if (discount > 0 || discount === 0) {
+    // if (discount > 0 || discount === 0) {
 
-      const timer = setTimeout(() => {
-        setShowDiscountStatus(false); // Hide the message after 2 seconds
-        setCouponCode('');
-      }, 2000);
+    //   const timer = setTimeout(() => {
+    //     setShowDiscountStatus(false); // Hide the message after 2 seconds
+    //     setCouponCode('');
+    //   }, 2000);
 
-      return () => clearTimeout(timer); // Cleanup the timer on unmount
-    }
+    //   return () => clearTimeout(timer); // Cleanup the timer on unmount
+    // }
   })
 
   function convertHtmlToJSON(htmlString) {
@@ -646,7 +659,7 @@ const ShowEventDetails = () => {
   const handleCouponApply = async () => {
     try {
       if (couponCode) {
-        if (!scheduleBatch.specialOfferEvent && (couponCode != preCouponCode)) {
+        if (!scheduleBatch.specialOfferEvent) {
           const response = await fetch(`${apiUrl}api/validate-coupon`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
@@ -1401,6 +1414,8 @@ const ShowEventDetails = () => {
                               </div>
                             </div>
                           }
+                          {showDiscountStatus && discount > 0 && <p style={{ 'display': 'flex', 'font-weight': 'bold', 'color': 'green', 'gap': '5px' }}> Discount Applied   <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill-rule="evenodd" clip-rule="evenodd" d="M21.007 8.27C22.194 9.125 23 10.45 23 12c0 1.55-.806 2.876-1.993 3.73.24 1.442-.134 2.958-1.227 4.05-1.095 1.095-2.61 1.459-4.046 1.225C14.883 22.196 13.546 23 12 23c-1.55 0-2.878-.807-3.731-1.996-1.438.235-2.954-.128-4.05-1.224-1.095-1.095-1.459-2.611-1.217-4.05C1.816 14.877 1 13.551 1 12s.816-2.878 2.002-3.73c-.242-1.439.122-2.955 1.218-4.05 1.093-1.094 2.61-1.467 4.057-1.227C9.125 1.804 10.453 1 12 1c1.545 0 2.88.803 3.732 1.993 1.442-.24 2.956.135 4.048 1.227 1.093 1.092 1.468 2.608 1.227 4.05Zm-4.426-.084a1 1 0 0 1 .233 1.395l-5 7a1 1 0 0 1-1.521.126l-3-3a1 1 0 0 1 1.414-1.414l2.165 2.165 4.314-6.04a1 1 0 0 1 1.395-.232Z" fill="#009912"></path></g></svg></p>}
+                          {showDiscountStatus && discount == 0 && <p style={{ 'display': 'flex', 'font-weight': 'bold', 'color': '#c70000', 'gap': '5px' }}> Please try to apply the coupon again. <svg width="24px" height="24px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill="#c70000" fill-rule="evenodd" d="M8,1 C11.8659932,1 15,4.13400675 15,8 C15,11.8659932 11.8659932,15 8,15 C4.13400675,15 1,11.8659932 1,8 C1,4.13400675 4.13400675,1 8,1 Z M3,8 C3,10.7614237 5.23857625,13 8,13 C9.01910722,13 9.96700318,12.6951083 10.7574478,12.1715651 L3.8284349,5.24255219 C3.30489166,6.03299682 3,6.98089278 3,8 Z M8,3 C6.98089278,3 6.03299682,3.30489166 5.24255219,3.8284349 L12.1715651,10.7574478 C12.6951083,9.96700318 13,9.01910722 13,8 C13,5.23857625 10.7614237,3 8,3 Z"></path> </g></svg></p>}
                           {selected && eventType == 'BackPackingTrip' &&
                             <div className='payment-selection'>
                               <label className='radio-display'>
@@ -1423,8 +1438,6 @@ const ShowEventDetails = () => {
                               </label>
                             </div>
                           }
-                          {showDiscountStatus && discount > 0 && <p style={{ 'display': 'flex', 'font-weight': 'bold', 'color': 'green', 'gap': '5px' }}> Discount Applied   <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill-rule="evenodd" clip-rule="evenodd" d="M21.007 8.27C22.194 9.125 23 10.45 23 12c0 1.55-.806 2.876-1.993 3.73.24 1.442-.134 2.958-1.227 4.05-1.095 1.095-2.61 1.459-4.046 1.225C14.883 22.196 13.546 23 12 23c-1.55 0-2.878-.807-3.731-1.996-1.438.235-2.954-.128-4.05-1.224-1.095-1.095-1.459-2.611-1.217-4.05C1.816 14.877 1 13.551 1 12s.816-2.878 2.002-3.73c-.242-1.439.122-2.955 1.218-4.05 1.093-1.094 2.61-1.467 4.057-1.227C9.125 1.804 10.453 1 12 1c1.545 0 2.88.803 3.732 1.993 1.442-.24 2.956.135 4.048 1.227 1.093 1.092 1.468 2.608 1.227 4.05Zm-4.426-.084a1 1 0 0 1 .233 1.395l-5 7a1 1 0 0 1-1.521.126l-3-3a1 1 0 0 1 1.414-1.414l2.165 2.165 4.314-6.04a1 1 0 0 1 1.395-.232Z" fill="#009912"></path></g></svg></p>}
-                          {showDiscountStatus && discount == 0 && <p style={{ 'display': 'flex', 'font-weight': 'bold', 'color': '#c70000', 'gap': '5px' }}> Coupon Not Applied  <svg width="24px" height="24px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill="#c70000" fill-rule="evenodd" d="M8,1 C11.8659932,1 15,4.13400675 15,8 C15,11.8659932 11.8659932,15 8,15 C4.13400675,15 1,11.8659932 1,8 C1,4.13400675 4.13400675,1 8,1 Z M3,8 C3,10.7614237 5.23857625,13 8,13 C9.01910722,13 9.96700318,12.6951083 10.7574478,12.1715651 L3.8284349,5.24255219 C3.30489166,6.03299682 3,6.98089278 3,8 Z M8,3 C6.98089278,3 6.03299682,3.30489166 5.24255219,3.8284349 L12.1715651,10.7574478 C12.6951083,9.96700318 13,9.01910722 13,8 C13,5.23857625 10.7614237,3 8,3 Z"></path> </g></svg></p>}
                           <div className='hr'></div>
                           {selected &&
                             <div className='finalCalculation'>
@@ -1545,13 +1558,13 @@ const ShowEventDetails = () => {
                             disabled={!showTermsAndConditions}
                           />
                           <div >
-                            Accept all
-                            <a className='link' href={'https://sahyadrivacations.com/user-agreement'} target="_blank"> terms & conditions</a>
+                           I accept all terms & conditions.{paynowButtonDisabled}
+                            <a className='link' href={'https://sahyadrivacations.com/user-agreement'} target="_blank"> (View)</a>
                           </div>
                         </div>
                         {errors.dateError && <p className='show-error' >{errors.dateError.message}</p>}
                         <div className="button">
-                          <input onMouseOver={handleCheckboxBlur} className={!showTermsAndConditions ? 'disable-paynow' : 'paynow-button'} disabled={!showTermsAndConditions} type="submit" value="Pay Now" />
+                          <input onMouseOver={handleCheckboxBlur} className={!paynowButtonDisabled ? 'disable-paynow' : 'paynow-button'} disabled={paynowButtonDisabled ? false: true } type="submit" value="Pay Now" />
                         </div>
                       </div>
                     }
